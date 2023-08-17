fb631da09e55d19755677a07a6cef77b8a037b6e
sandhya <120093615+medapatisandhya@users.noreply.github.com>
2023-08-17 11:38:24 +0530

    import java.util.Scanner; public class Bill {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n1,n2,x,m;         n1=sc.nextInt();         n2=sc.nextInt();         x=sc.nextInt();         m=((x+n1)/n2)+1;         if(n2<n1)         {             System.out.println("-1");         }         else         {             System.out.println(m);         }
        }
    }

---
+++ Cars.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Bill
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1,n2,x,m;
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        x=sc.nextInt();
+        m=((x+n1)/n2)+1;
+        if(n2<n1)
+        {
+            System.out.println("-1");
+        }
+        else
+        {
+            System.out.println(m);
+        }
+        
+    }
+}
