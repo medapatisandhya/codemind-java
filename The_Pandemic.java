674fb3ca3bf0cca54ed2d2e7cfe080f5cadee85a
sandhya <120093615+medapatisandhya@users.noreply.github.com>
2023-08-17 11:48:32 +0530

    import java.util.Scanner; public class River {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n;         n=sc.nextInt();         if(n<3)         {             System.out.println(n-1);         }         else         {             System.out.println(2);         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class River
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n<3)
+        {
+            System.out.println(n-1);
+        }
+        else
+        {
+            System.out.println(2);
+        }
+    }
+}
