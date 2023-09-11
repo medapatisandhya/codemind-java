d9f800b58b4088da989f07583cf71ee99bb5d09e
sandhya <120093615+medapatisandhya@users.noreply.github.com>
2023-09-11 15:18:17 +0530

    import java.util.Scanner; public class A {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int t,s,b,m;         t=sc.nextInt();         s=sc.nextInt();         b=sc.nextInt();         m=(2*t*s*b*512)/1024;         System.out.printf("%d KB",m);     } }

--- Capacity.java
+++ Capacity.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class Capacity
+public class A
 {
     public static void main(String args[])
     {
-        Scanner sc= new Scanner(System.in);
-        int t,s,b,c;
+        Scanner sc = new Scanner(System.in);
+        int t,s,b,m;
         t=sc.nextInt();
         s=sc.nextInt();
         b=sc.nextInt();
-        c=2*t*b*s*512/1024;
-        System.out.printf("%d KB",c);
+        m=(2*t*s*b*512)/1024;
+        System.out.printf("%d KB",m);
     }
 }
